a7d5de223e99574095efd037fb35d2b66a133c01
Joshua-Simon <joshy.d.simon@gmail.com>
2020-05-10 15:01:08 +1000

    styling

---
+++ app.js
@@ -0,0 +1,10 @@
+const firstNum = document.querySelector('[data-num-one]')
+const secondNum = document.querySelector('[data-num-two')
+
+firstNum.addEventListener('input', () => {
+    console.log('number 1 it got here')
+})
+
+secondNum.addEventListener('input', () => {
+    console.log('number 2 it got here')
+})
